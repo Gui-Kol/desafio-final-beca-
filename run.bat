a760259fba1c839b01c420ec060ff347415ecbe3
Gui-Kol <kolnd4@gmail.com>
2026-01-29 12:27:05 -0300

    Adição de run.bat para rodar os programas sem o mvn clean install

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ================================
+echo Starting services...
+echo ================================
+
+start "service-client" java -jar service-client/target/service-client-0.0.1-SNAPSHOT.jar
+start "service-transaction" java -jar service-transaction/target/service-transaction-0.0.1-SNAPSHOT.jar
+start "service-transaction-processor" java -jar service-transaction-processor/target/service-transaction-processor-0.0.1-SNAPSHOT.jar
+
+echo ================================
+echo All services have been initiated.
+echo ================================
+pause
